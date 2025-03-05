907397371da8e17374da9a70cfe012711a66cd58
jelargeron <jelargeron@a110pc14L.local.isima.fr>
2025-03-05 16:19:13 +0100

    add fibonacci

---
+++ Fibonacci/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <rabbitPop.h> 
+
+#define NB_SIMULATION 100
+
+
+/*##############################################*
+*                                               *
+*                                               *
+*                                               *
+*###############################################*/
+
+
+int main (int argc,char* argv){
+
+    rabbitPop rabbit_population;
+
+    rabbit_population.nb_Rabbit_Addult = 1;
+
+    fibbonnaci(100,rabbit_population);
+
+
+}
---
+++ Fibonacci/rabbitPop.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <rabbitPop.h> 
+
+
+typedef struct {
+    double nb_Rabbit_Adult;
+    double nb_Rabbit_Enfant;
+}rabbitPop;
+
+
+
+
+/*##############################################*
+*   @brief:  fibbonacci est une fonction qui    *
+*            calcule le nombre de couple de     *
+*            lapin à chaque étape (mois)        *                              
+*                                               *
+*   @return: une structure de population de     *
+*            couple de lapin                    *                                               *
+*###############################################*/
+
+
+
+rabbitPop fibbonacci(int nb_of_time,rabbitPop population_of_rabbit){
+
+    double temp;
+
+    for (int i = 0 ; i < nb_of_time; i++){
+
+        temp = population_of_rabbit.nb_Rabbit_Enfant;
+        population_of_rabbit.nb_Rabbit_Enfant = nb_Rabbit_Adult;
+        population_of_rabbit.nb_Rabbit_Adult += temp;
+
+    }
+
+    return population_of_rabbit;
+
+}
